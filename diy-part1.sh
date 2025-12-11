1a06a5ecaf0e97364937fda634b033636add3144
1303045940 <1332338040@qq.com>
2025-12-11 19:18:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,25 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
+#修改WIFI名称
+sed -i "s/ImmortalWrt/Openwrt/g" $WIFI_FILE
+#修改WIFI加密
+sed -i "s/encryption=.*/encryption='psk2+ccmp'/g" $WIFI_FILE
+#修改WIFI密码
+sed -i "/set wireless.default_\${dev}.encryption='psk2+ccmp'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='password'" $WIFI_FILE
+CFG_FILE="./package/base-files/files/bin/config_generate"
+#修改默认IP地址
+#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='OpenWrt'/g" $CFG_FILE
+#添加第三方软件源
+sed -i "s/option check_signature/# option check_signature/g" package/system/opkg/Makefile
+echo src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/aarch64_cortex-a53/kiddin9 >> ./package/system/opkg/files/customfeeds.conf
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
