adb5bc5e7c300080d3353a29a9077b37dc46860a
1303045940 <1332338040@qq.com>
2025-12-27 12:25:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-sed -i '$a src-git small https://github.com/1303045940/Kwrt' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/1303045940/Kwrt' feeds.conf.default
 
 
 #添加第三方软件源
