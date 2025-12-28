24da05d9b275f00dad899d28cded5d07ef233f57
1303045940 <1332338040@qq.com>
2025-12-29 00:05:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,11 @@
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# 在 diy-part2.sh 中执行，此时 feeds 已经下载完毕
+# 强制删除所有包含 vsean.net 的软件源行
+find ./ -name "distfeeds.conf" | xargs sed -i '/vsean.net/d'
+find ./ -name "distfeeds.conf" | xargs sed -i '/immortalwrt_kiddin9/d'
+
+# 针对具体已知的顽固路径进行清理
+[ -f package/mtk/applications/luci-app-openfi/root/usr/opkg/distfeeds.conf ] && sed -i '/vsean.net/d' package/mtk/applications/luci-app-openfi/root/usr/opkg/distfeeds.conf
