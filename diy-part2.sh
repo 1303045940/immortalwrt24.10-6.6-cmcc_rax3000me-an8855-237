21c7cf8cdc60d2dce12b56a8acb80ab3ab051038
1303045940 <1332338040@qq.com>
2025-12-29 00:07:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,11 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # 在 diy-part2.sh 中执行，此时 feeds 已经下载完毕
-# 强制删除所有包含 vsean.net 的软件源行
+
+# 全局扫描：删除所有配置文件中包含 vsean.net 的行，但不触碰其他官方源
 find ./ -name "distfeeds.conf" | xargs sed -i '/vsean.net/d'
 find ./ -name "distfeeds.conf" | xargs sed -i '/immortalwrt_kiddin9/d'
+
+# 针对你提到的那个 MTK 插件路径进行精准清理
+[ -f package/mtk/applications/luci-app-openfi/root/usr/opkg/distfeeds.conf ] && \
+sed -i '/vsean.net/d' package/mtk/applications/luci-app-openfi/root/usr/opkg/distfeeds.conf
